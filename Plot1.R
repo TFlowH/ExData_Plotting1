26b30c662d83b5c4d62877a55d0a5807cb32d7e7
TFlowH <60362751+TFlowH@users.noreply.github.com>
2020-04-03 21:38:05 +0200

    Update Plot1.R

--- Plot1.R
+++ Plot1.R
@@ -23,7 +23,7 @@ library(readr)
 ##################################################################
 ##                       Start the Plot                         ##
 ##################################################################
-png(file="plot1.png",width=480,height=480)
+png(file="03_Output/plot1.png",width=480,height=480)
 
 ##---------------------------------------------------------------
 ##                   Create Histogram                           -
@@ -32,6 +32,6 @@ hist(data$Global_active_power, col = "red", ylab = "Frequency",
      xlab = "Global Active Power (kilowatts)", main = "Global Active Power ")
 
 ##---------------------------------------------------------------
-##                   CEnd the Plot                              -
+##                    End the Plot                              -
 ##---------------------------------------------------------------
 dev.off()
